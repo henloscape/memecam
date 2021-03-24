ddcb4b2e26265db7c85ea43e27401b0fc486ba33
JamieHoSzeYui <henloboii@protonmail.ch>
2021-03-24 08:17:07 +0800

    MiuiCamera: Initialize MiuiCamera
    Highly inspired by https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.
    This can be used for devices using prebuilt vendor (non OSS) as I got rid of most libs and vendor stuff that we already have.
    
    Co-authored-by: Aryan Sinha <sinha.aryan03@gmail.com>
    Co-authored-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ config.mk
@@ -0,0 +1,14 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/miuicamera
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/bin,$(TARGET_COPY_OUT_SYSTEM)/bin) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
+
+PRODUCT_PACKAGES += \
+    MiuiCamera \
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.miui.notch=1
